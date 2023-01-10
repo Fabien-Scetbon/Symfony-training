4ccaa44b17895db3d9f0100c1f6a57d9a3240e0b
Fabien-Scetbon <fscetbon@gmail.com>
2023-01-10 12:28:53 +0100

    route

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class SessionController extends AbstractController
+{
+    #[Route('/session', name: 'app_session')]
+    public function index(Request $request): Response
+    {
+        return $this->render('session/index.html.twig', [
+            'controller_name' => 'SessionController',
+        ]);
+    }
+}
