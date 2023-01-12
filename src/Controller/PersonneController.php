3ecbc8e3f2e6c6b923bdecb00130ea8739c52989
Fabien-Scetbon <fscetbon@gmail.com>
2023-01-12 11:42:11 +0100

    liste des personnes

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -8,9 +8,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/personne/add', name: 'personne.add')]
+    #[Route('/', name: 'personne.list')]
+    public function index(ManagerRegistry $doctrine): Response {
+        $repository = $doctrine->getRepository( persistentObject: Personne::class);
+        $personnes = $repository->findAll();
+        return $this->render('personne/index.html.twig',
+        ['personnes' => $personnes]
+    );
+    }
+
+    #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response    // $doctrine ou autre nom
     {
         $entityManager = $doctrine->getManager();
@@ -22,6 +32,7 @@ class PersonneController extends AbstractController
 
         // ajouter operation insertion dans la transaction
         $entityManager->persist($personne); // valable pour ajout ou modif suivant si l'id existe ou pas
+        // on aurait pu ajouter d'autres personnes .....
 
         // execute la transaction
         $entityManager->flush();
