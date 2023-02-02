d6c616e6c67a5abb07589829ceb8bad8a1df9a83
Fabien-Scetbon <fscetbon@gmail.com>
2023-02-02 09:34:14 +0100

    auth

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,10 +4,11 @@ namespace App\DataFixtures;
 
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class UserFixture extends Fixture
+class UserFixture extends Fixture implements FixtureGroupInterface
 {
     public function __construct(
         private UserPasswordHasherInterface $hasher
@@ -15,14 +16,34 @@ class UserFixture extends Fixture
     {
         
     }
+
     public function load(ObjectManager $manager): void
     {
         $admin1 = new User();
         $admin1->setEmail(email:'admin1@gmail.com');
+        $admin1->setPassword('admin');
+        $admin1->setRoles(['ROLE_ADMIN']);
 
         $admin2 = new User();
         $admin2->setEmail(email:'admin2@gmail.com');
+        $admin2->setPassword($this->hasher->hashPassword($admin2, 'admin'));
+        $admin2->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin1);
+        $manager->persist($admin2);
+
+        for($i=1; $i<=5; $i++) {
+            $user = new User();
+            $user->setEmail(email:"user$i@gmail.com");
+            $user->setPassword($this->hasher->hashPassword($user, 'user'));
+            $manager->persist($user);
+        }
 
         $manager->flush();
+    }
+
+    public static function getGroups(): array
+    {
+        return ['user'];
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -26,6 +28,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function getId(): ?int
     {
@@ -93,6 +98,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
